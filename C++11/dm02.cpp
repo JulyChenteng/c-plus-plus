c2218eab063af4ff27b9ded0e3c077a69070dd38
JulyChenteng <393673587@qq.com>
2020-11-24 14:09:57 +0800

    update

--- C++11/dm02.cpp
+++ C++11/dm02.cpp
@@ -1,6 +1,7 @@
 //! g++ dm02.cpp -I "F:\\Open Source\\boost_1_73_0"
 //! 基础概念：左值、右值、引用折叠
 
+//! 万能引用
 #if 0
 #include <iostream>
 #include <boost/type_index.hpp>
@@ -174,7 +175,7 @@ output:
 
 #endif
 
-#if 1
+#if 0
 
 #include <iostream>
 #include <cstring>
@@ -209,3 +210,38 @@ int main()
 }
 
 #endif
+
+
+#if 1 
+#include <iostream>
+#include <boost/type_index.hpp>
+
+using namespace std;
+using boost::typeindex::type_id_with_cvr;  
+
+template<typename T>
+void PrintType(T param)
+{
+    // 利用Boost库打印模板推导出来的 T 类型
+	cout << "T type: " << type_id_with_cvr<T>().pretty_name() << endl; 
+    
+    // 利用Boost库打印形参的类型
+	cout << "param type:" << type_id_with_cvr<decltype(param)>().pretty_name() << endl;
+}
+
+int main()
+{
+	int a = 0;                              // 左值
+	PrintType(a);                           // 传入左值
+
+	int &lvalue_refence_a = a;              // 左值引用
+	PrintType(lvalue_refence_a);            // 传入左值引用
+
+	PrintType(int(2));                      // 传入右值
+
+    const char* const ptr = "fun test";
+    PrintType(ptr);
+}
+#endif
+
+
